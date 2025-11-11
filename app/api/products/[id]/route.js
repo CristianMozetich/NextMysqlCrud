74c798efdea75cfbd0cb068c28f5c91b72d9c0fd
Cristian Mozetich <cristianmozetich@gmail.com>
2025-11-11 11:41:57 +0000

    get by id

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -1,22 +1,32 @@
 import { NextResponse } from "next/server";
+import { conn } from "@/libs/mysql";
 
-export function GET() {
+export async function GET(request, context) {
+  try {
+    const params = await context.params;
+    const result = await conn.query("SELECT * FROM product WHERE id = ?", [
+      params.id,
+    ]);
+    if (result.length === 0) {
+      return NextResponse.json("producto no encontrado", { status: 404 });
+    }
+    return NextResponse.json(result[0]);
+  } catch (error) {
     return NextResponse.json(
-        
-        'obteniendo producto'
-    )
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
 
 export function DELETE() {
-    return NextResponse.json(
-        
-        'eliminando producto'
-    )
+  return NextResponse.json("eliminando producto");
 }
 
 export function PUT() {
-    return NextResponse.json(
-        
-        'actualizando producto'
-    )
+  return NextResponse.json("actualizando producto");
 }
