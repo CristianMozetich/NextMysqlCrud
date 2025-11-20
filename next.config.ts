d23addd61a62c7ec783709b5dfdf61d2aa1c1d0d
Cristian Mozetich <cristianmozetich@gmail.com>
2025-11-20 00:18:32 +0000

    next config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/",
-        destination: "/products",
+        destination: "/",
         permanent: true,        
       },
     ]
